9f6e79a8a0f2b4028f6b7b483cec744a8e9305a9
Thewhey-Brian <xinyuguo@wustl.edu>
2021-10-26 16:06:02 -0400

    Initial Updates

--- Reference/get_dicts.py
+++
@@ -1,9 +0,0 @@
-from tabula.io import read_pdf
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-
-df  = pd.read_html('http://adni.loni.usc.edu/data-samples/adni-data-inventory/')
-df = pd.DataFrame(df[0])
-adni_inv = df.iloc[:, 0:3]
-adni_inv.to_csv('adni_inv.csv', index=False)
